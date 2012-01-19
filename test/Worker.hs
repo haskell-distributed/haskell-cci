a09e67f8f7e0d053ba56d398d1c8337fae2bc0ae
Facundo Domínguez <facundo.dominguez@parsci.com>
2012-01-19 05:49:10 -0700

    Changes license to BSD3.

--- test/Worker.hs
+++ test/Worker.hs
@@ -1,19 +1,7 @@
 --
--- Copyright (C) 2012 Parallel Scientific 
+-- Copyright (C) 2012 Parallel Scientific. All rights reserved.
 --
--- This file is part of cci-haskell.
---
--- cci-haskell is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License Version 2 as 
--- published by the Free Software Foundation.
---
--- cci-haskell is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with cci-haskell.  If not, see <http://www.gnu.org/licenses/>.
+-- See the accompanying COPYING file for license information.
 --
 
 import Prelude hiding          ( catch )
